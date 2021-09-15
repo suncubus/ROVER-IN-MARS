1e1f05e4172ff35770fc3909f22994f607ac2f68
Patri <witty.free@gmail.com>
2021-09-15 19:04:35 +0200

    Añadir campos a las interfaces

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,10 @@
+export interface Area {
+    areaX   :number
+    areaY   :number
+}
+export interface Rover {
+    roverX          :number
+    roverY          :number
+    roverOrientation:string
+    roverCommands   :string[]
+}
